fdca1bf5c6f21650cd71aaafec4ff3aee14ca603
Skchouhan753 <s.k.chouhan753@gmail.com>
2024-03-28 00:54:37 +0530

    Debug-Dhata V 1.0

---
+++ Backend/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const cors = require("cors");
+
+require("dotenv").config();
+
+const PORT = process.env.port;
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+app.listen(PORT, async () => {
+  try {
+    await connection;
+    console.log(`server started at port ${PORT}`);
+    console.log("connected to DB");
+  } catch (err) {
+    console.log(err);
+  }
+});
