0199281908c5e82c8496e130413c21d07ad547fa
Deepak Underiya <underiyadeepak@gmail.com>
2024-03-29 12:21:28 +0530

    footer

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,15 @@
-
-import './App.css'
+import "./App.css";
+import Footer from "./components/Footer";
+// import AppPage from "./pages/AppsPage";
 
 function App() {
-  
-
   return (
     <>
       <div>
-        
+        <Footer />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
