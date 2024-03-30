ef23669ca39bf2f3665ee014d6a24722c43da719
Yogesh <kushawahyogesh93@gmail.com>
2024-03-30 15:04:46 +0530

    mostly home stuff i fixed

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,11 @@
+import Homepage from "./Homepage/Homepage";
 import Footer from "./components/Footer";
-import Hompage from "./components/Homepage/Hompage";
 function App() {
   return (
     <>
       <div>
         {/* <Navbar /> */}
-        <Hompage />
+        <Homepage/>
         <Footer />
       </div>
     </>
---
+++ Frontend/src/Homepage/Homepage.jsx
@@ -0,0 +1,20 @@
+import DailyPlan from "./DailyPlan"
+import Network from "./Network"
+import OurPhilosophy from "./OurPhilospy"
+import QA from "./QA"
+import TalkWithExpert from "./TalkWithExpert"
+
+function Homepage() {
+  return (
+    <div>
+      <DailyPlan />
+
+      <OurPhilosophy />
+      <Network />
+      <QA />
+      <TalkWithExpert />
+    </div>
+  );
+}
+
+export default Homepage
