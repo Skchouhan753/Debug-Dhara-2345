6bef4d8b8896f43dcf385c171f99f40388877cfc
Skchouhan753 <s.k.chouhan753@gmail.com>
2024-04-04 18:42:32 +0530

    issue resolved

--- Frontend/src/redux/auth/login/actionsLogin.js
+++ Frontend/src/redux/auth/login/actionsLogin.js
@@ -1,3 +1,4 @@
+import { createGlobalStyle } from "styled-components";
 import {
   LOGIN_FAILURE,
   LOGIN_LOADING,
@@ -21,11 +22,28 @@ export const loginAPI = (creds) => (dispatch) => {
   };
 
   axios
-    .post("https://myfitnesspalclone17.herokuapp.com/auth/login", data, {
+    .post("https://debug-dhara-backend-server.onrender.com/users/login", data, {
       headers: headers,
     })
     .then((r) => dispatch({ type: LOGIN_SUCCESS, payload: r.data }))
     .catch(() => dispatch({ type: LOGIN_FAILURE }));
 };
 
-export const logoutAPI = () => ({ type: LOGOUT });
+// export const logoutAPI = () => (
+//   dispatch({ type: LOGOUT })
+//   return axios
+//     .delete(`https://debug-dhara-backend-server.onrender.com/snack/${payload}`)
+//     .then((res) =>
+//       dispatch({ type: types.DELETE_SNACK_SUCCESS, payload: res.data })
+//     )
+//     .catch((err) => dispatch({ type: types.DELETE_SNACK_FAILURE }));
+//   );
+export const logoutAPI = () => (dispatch) => {
+  dispatch({ type: LOGOUT });
+  return axios
+    .get(`https://debug-dhara-backend-server.onrender.com/users/logout`)
+    .then((res) =>
+      console.log(res)
+    )
+    .catch((err) => console.log(err));
+};
--- Frontend/src/redux/auth/register/actionsRegister.js
+++ Frontend/src/redux/auth/register/actionsRegister.js
@@ -9,24 +9,62 @@ export const registerAPI = (creds) => (dispatch) => {
   dispatch({ type: REGISTER_LOADING });
 
   let data = {
-    // name: creds.name,
+    username: creds.username,
     email: creds.email,
     password: creds.password,
-    // username: creds.username,
   };
 
   // localStorage.setItem("nameUser", JSON.stringify(creds.name));
 
-  data = JSON.stringify(data);
+  // data = JSON.stringify(data);
 
   const headers = {
     "Content-Type": "application/json",
   };
-
+// https://myfitnesspalclone17.herokuapp.com/auth/register
   axios
-    .post("https://myfitnesspalclone17.herokuapp.com/auth/register", data, {
+    .post("https://debug-dhara-backend-server.onrender.com/users/register", data, {
       headers: headers,
     })
     .then((r) => dispatch({ type: REGISTER_SUCCESS, payload: r.data }))
     .catch((e) => dispatch({ type: REGISTER_FAILURE, payload: e }));
 };
+
+
+
+// export const registerAPI = (creds) => (dispatch) => {
+//   dispatch({ type: REGISTER_LOADING });
+
+//   let data = {
+//     username: creds.username,
+//     email: creds.email,
+//     password: creds.password,
+//   };
+
+//   // Convert data to JSON format
+//   // data = JSON.stringify(data);
+
+//   const headers = {
+//     "Content-Type": "application/json",
+//   };
+
+
+//   axios
+//     .post("https://debug-dhara-backend-server.onrender.com/users/register", data, {
+//       headers: headers,
+//     })
+//     .then((response) => {
+//       // Assuming the response contains the user data
+//       dispatch({ type: REGISTER_SUCCESS, payload: response.data });
+//       // console.log(response)
+//     })
+//     .catch((error) => {
+//       // Handle the error properly
+//       let errorMessage = "an error occurred during registration.";
+//       if (error.response && error.response.data && error.response.data.message) {
+//         errorMessage = error.response.data.message;
+//       }
+//       dispatch({ type: REGISTER_FAILURE, payload: errorMessage });
+//     });
+// };
+
